2233774333899e1d09228c8333cbca2a39d0aac2
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-25 22:43:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ if array.length == 1 
+   array.join("")
+   
+   elsif array.length == 2 
+   array.join(" and ")
+   
+   elsif array.length >= 3
+   last_array_item = array[-1]
+   array[-1] = "and #{last_array_item}"
+   array.join(", ")
+ end 
 end
