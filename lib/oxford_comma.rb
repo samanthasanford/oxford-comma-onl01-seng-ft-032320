1a1a8e49b03ca342bbed0e169f4e206919438637
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-25 22:26:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,11 @@
 def oxford_comma(array)
-  if array.length == 1 
-    array.join("")
+  if array.size == 1 
+    return array[0]
     
-  elsif array.length == 2 
-  array.join(" and ")
-
-  
-  elsif array.length == 3
-  array[-1].insert (0, "and")
-  array.join(", ")
-  
-  
-  elsif array.length > 3
-  array[-1].insert(0, "and ")
-  array.join(", ")
+    elsif array.size == 2 
+    return array.join(" and ")
+    
+  else array.size == 3
+    return array[0..-2].join(", ") + ", and" + array[-1]
+  end 
 end
