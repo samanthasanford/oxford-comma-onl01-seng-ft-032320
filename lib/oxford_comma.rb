3faf08eb80f5def6109a2c9bf38790d61de5c60f
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-25 22:20:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,15 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join("")
-    #without additional formatting for 1 element array
     
   elsif array.length == 2 
   array.join(" and ")
-  #when given a two element array add "and"
+
   
   elsif array.length == 3
   array[-1].insert (0, "and")
   array.join(", ")
-  #three element array with a final and between last two elements) and commas
+  
   
   elsif array.length > 3
   array[-1].insert(0, "and ")
